8c56005aac042a5070414521f704bee5e26a86aa
gauravano <sachdeva.gaurav1997@gmail.com>
2017-11-17 13:08:08 +0530

    Tracks parsed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
 import React ,{ Component } from 'react';
 import {FormGroup,FormControl,InputGroup,Glyphicon} from 'react-bootstrap';
 import './App.css';
-
+import Profile from './Profile';
 class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+        query: '',
+        artist: null,
+        tracks:[]
+    }
+  }
+
+  search(){
+    console.log('this.state',this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    const ALBUM_URL = "https://api.spotify.com/v1/artists/";
+
+    console.log("FETCH_URL",FETCH_URL);
+
+    fetch(FETCH_URL,{
+      method:'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.item[0];
+      console.log("artist",artist);
+      this.setState({artist});
+
+      FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+      fetch(FETCH_URL,{
+        method:'GET'
+      })
+      .then(response => response.json())
+      .then(json => {
+        console.log('artist \'s top tracks',json);
+        const {tracks} = json;
+        this.setState({tracks});
+      })
+
+    })
+  }
+
   render(){
     return(
       <div className="App" >
@@ -12,19 +53,31 @@ class App extends Component{
             <FormControl
               type="text"
               placeholder="Search an Artist"
+              value = {this.state.query}
+              onChange = {event => {this.setState({query:event.target.value})}}
+              onKeyPress = {event => {
+                if(event.key === 'Enter'){
+                  this.search();
+                }
+              }}
               />
-            <InputGroup.Addon>
+            <InputGroup.Addon onClick={()=>this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
+        {
+          this.state.artist !== null ?
+            <div>
+              <Profile
+                  artist={this.state.artist}
+                />
+              <div className="Gallery">Gallery</div>
+            </div>
 
-        <div className="Profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
-        </div>
-        <div className="Gallery">Gallery</div>
-      </div>
+            :<div></div>
+        }
+    </div>
     );
   }
 }
